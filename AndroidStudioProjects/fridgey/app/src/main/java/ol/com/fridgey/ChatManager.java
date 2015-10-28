ef0312b42cf2c932c7f74482381a5be2f8bd9e65
isaw25 <swasii25@gmail.com>
2015-10-28 13:42:57 +0530

    voice+aiml integrated

---
+++ AndroidStudioProjects/fridgey/app/src/main/java/ol/com/fridgey/ChatManager.java
@@ -0,0 +1,7 @@
+package ol.com.fridgey;
+
+/**
+ * Created by imran on 26-10-2015.
+ */
+public class ChatManager {
+}
---
+++ AndroidStudioProjects/fridgey/app/src/main/java/ol/com/fridgey/ZipFileExtraction.java
@@ -0,0 +1,7 @@
+package ol.com.fridgey;
+
+/**
+ * Created by imran on 27-10-2015.
+ */
+public class ZipFileExtraction {
+}
